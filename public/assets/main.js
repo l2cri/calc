b0b3cd2d51d0f806c5f253a51440c9f6959e7c32
l2cri <gl2cri@gmail.com>
2016-11-09 16:40:41 +0300

    most beautiful

--- public/assets/main.js
+++ public/assets/main.js
@@ -24,14 +24,39 @@ $(function() {
                 digits: 'пожалуйста, вводите только цифры'
             }
         },
-        errorPlacement: function (error, element) {
+        errorElement: "em",
+        errorPlacement: function ( error, element ) {
+            // Add the `help-block` class to the error element
+            error.addClass( "help-block" );
 
-            $(element).parents('.form-group').addClass('has-error');
+            // Add `has-feedback` class to the parent div.form-group
+            // in order to add icons to inputs
+            element.parents( ".col-sm-5" ).addClass( "has-feedback" );
 
-            error.insertAfter(element).addClass('control-label');
+            if ( element.prop( "type" ) === "checkbox" ) {
+                error.insertAfter( element.parent( "label" ) );
+            } else {
+                error.insertAfter( element );
+            }
+
+            // Add the span element, if doesn't exists, and apply the icon classes to it.
+            if ( !element.next( "span" )[ 0 ] ) {
+                $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+            }
         },
-        success: function(error, element){
-            $(element).parents('.form-group').addClass('has-success').removeClass('has-error');
+        success: function ( label, element ) {
+            // Add the span element, if doesn't exists, and apply the icon classes to it.
+            if ( !$( element ).next( "span" )[ 0 ] ) {
+                $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+            }
+        },
+        highlight: function ( element, errorClass, validClass ) {
+            $( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+            $( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+        },
+        unhighlight: function ( element, errorClass, validClass ) {
+            $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+            $( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
         },
         submitHandler: function(form){
             var _this = $(form);
@@ -45,7 +70,7 @@ $(function() {
                 $result.html(res.result);
             })
             .fail(function(jqXHR) {
-                $result.replaceWith("Ошибка: "+jqXHR.responseText);
+                $result.html("Ошибка");
             });
         }
     })
--- resources/views/form/calc.blade.php
+++ resources/views/form/calc.blade.php
@@ -1,44 +1,58 @@
 @extends('app')
 
 @section('content')
-    <div class="header">
-        <h1>Калькулятор</h1>
-    </div>
+<div class="row">
+    <div class="col-sm-8 col-sm-offset-2">
 
-    <div class="jumbotron">
-        {!! Form::open(['url'=>'execute-calc/','method'=>'PATCH','class'=>'form-horizontal','id'=>'form-calc']) !!}
-        <div class="row">
-            <div class="col-sm-6 form-group">
-                {!! Form::label('num1','Первое число:',['class'=>'col-sm-4 control-label']) !!}
-                <div class="col-sm-8">
-                    {!! Form::text('num1',null,['class'=>'form-control']) !!}
-                </div>
-            </div>
-            <div class="col-sm-6 form-group">
-                {!! Form::label('num2','Второе число:',['class'=>'col-sm-4 control-label']) !!}
-                <div class="col-sm-8">
-                    {!! Form::text('num2',null,['class'=>'form-control']) !!}
-                </div>
+        <div class="page-header">
+            <div class="alert alert-info" role="alert">
+                <h4>Приложение использует следующие элементы:</h4>
+                <ul>
+                    <li><a href="https://laravel.com/" class="alert-link">Laravel</a>.</li>
+                    <li><a href="http://getbootstrap.com/" class="alert-link">Bootstrap</a>.</li>
+                    <li><a href="https://jqueryvalidation.org/" class="alert-link">jQuery Validation Plugin</a>.</li>
+                </ul>
             </div>
         </div>
-        <div class="row">
-            <div class="form-group">
-                <div class="col-sm-12">
-                    {!! Form::label('operation','Действие:',['class'=>'col-sm-1 control-label']) !!}
-                    <div class="col-sm-11">
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Калькулятор</h3>
+            </div>
+            <div class="panel-body">
+                {!! Form::open(['url'=>'execute-calc/','method'=>'PATCH','class'=>'form-horizontal','id'=>'form-calc']) !!}
+                <div class="form-group">
+                    {!! Form::label('num1','Первое число:',['class'=>'col-sm-4 control-label']) !!}
+                    <div class="col-sm-5">
+                        {!! Form::text('num1',null,['class'=>'form-control']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    {!! Form::label('num2','Второе число:',['class'=>'col-sm-4 control-label']) !!}
+                    <div class="col-sm-5">
+                        {!! Form::text('num2',null,['class'=>'form-control']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                    {!! Form::label('operation','Действие:',['class'=>'col-sm-4 control-label']) !!}
+                    <div class="col-sm-5">
                         {!! Form::select('operation', ['sum'=>'Сложение','sub'=>'Вычитание','multi'=>'Умножение',"degree"=>"Деление"],null,['class'=>'form-control']) !!}
                     </div>
                 </div>
-            </div>
+                <div class="form-group">
+                    {!! Form::label('result','Результат:',['class'=>'col-sm-4 control-label']) !!}
+                    <div class="col-sm-5">
+                        <div id="result-calc" class="form-control"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-5 col-sm-offset-4">
+                        {!! Form::submit('Рассчитать',['class'=>'btn btn-primary']) !!}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+             </div>
         </div>
-        <div class="form-group">
-            <div class="col-sm-9">
-                <div id="result-calc"  class="form-control"></div>
-            </div>
-            <div class="col-sm-3">
-                {!! Form::submit('Рассчитать',['class'=>'btn btn-primary pull-right']) !!}
-            </div>
-        </div>
-        {!! Form::close() !!}
     </div>
+</div>
 @stop
